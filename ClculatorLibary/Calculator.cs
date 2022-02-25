351ea63c324df2ca01e3c00356cab4030289e101
imdmehdi <imdmehdi@hotmail.com>
2022-02-26 02:41:13 +0530

    Calculator using base class lib

---
+++ ClculatorLibary/Calculator.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Linq;
+
+namespace ClculatorLibary
+{
+    public sealed class Calculator : ICalculator
+    {
+       
+        double ICalculator.Add(double firstNumber, double secondNumber)
+        {
+            try
+            {
+                return firstNumber + secondNumber;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+           
+        }
+        public void DoWok()
+        {
+            Console.WriteLine("hbjh");
+        }
+        double ICalculator.Divide(double firstNumber, double secondNumber)
+        {
+            try
+            {
+                return firstNumber / secondNumber;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        double ICalculator.Multiply(double firstNumber, double secondNumber)
+        {
+            try
+            {
+                
+                return firstNumber * secondNumber;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        double ICalculator.Substract(double firstNumber, double secondNumber)
+        {
+            try
+            {
+                
+                return firstNumber - secondNumber;
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+
+    public interface ICalculator
+    {
+        public double Add(double firstNumber, double secondNumber);
+        public  double Substract(double firstNumber, double secondNumber);
+        public double Multiply(double firstNumber, double secondNumber);
+        public double Divide(double firstNumber, double secondNumber);
+
+    }
+}
