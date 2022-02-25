6def224d08f28a00b70fbbbfafabbd8c7fcbf7e0
imdmehdi <imdmehdi@hotmail.com>
2022-02-25 19:01:31 +0530

    change

--- MemoryManagementAssingment/Program.cs
+++ MemoryManagementAssingment/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
 
 namespace MemoryManagementAssingment
 {
@@ -7,7 +9,9 @@ namespace MemoryManagementAssingment
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            #region code related to memory management assingment 1.
             {
+
                 Console.WriteLine("Total Memory: {0}", GC.GetTotalMemory(false));
                 Program p = new Program();
                 p.MakeObject();
@@ -30,8 +34,18 @@ namespace MemoryManagementAssingment
 
                 Console.WriteLine("Generation: {0}", GC.GetGeneration(p));
                 Console.WriteLine("Total Memory: {0}", GC.GetTotalMemory(false));
-
             }
+            #endregion
+
+
+            #region code related to assingment 2
+            {
+                IntPtr hwnd = new IntPtr(3);
+                UnManagedWork unManagedWork = new UnManagedWork(Process.GetCurrentProcess().MainWindowHandle);
+                unManagedWork.Dispose();
+            }
+
+            #endregion
             Console.ReadLine();
         }
         void MakeObject()
@@ -61,4 +75,46 @@ namespace MemoryManagementAssingment
 
     }
 
+    public class UnManagedWork : IDisposable
+    {
+        private IntPtr handle;
+        private Component component = new Component();
+        private bool isDisposed = false;
+
+        
+        public UnManagedWork(IntPtr handle)
+        {
+            this.handle = handle;
+        }
+
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.isDisposed)
+            {
+                if (disposing)
+                {
+                    component.Dispose();
+                }
+
+                CloseHandle(handle);
+                handle = IntPtr.Zero;
+
+                isDisposed = true;
+            }
+        }
+
+        [System.Runtime.InteropServices.DllImport("Kernel32")]
+        private extern static Boolean CloseHandle(IntPtr handle);
+        ~UnManagedWork()
+        {
+            Dispose(disposing: false);
+        }
+    }
+    
 }
